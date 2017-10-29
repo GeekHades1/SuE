0baad55456e1cc865b00fbb8d4738c3d2fc6b165
Hades <geekHades1@gmail.com>
2017-10-29 21:03:47 +0800

    Hades 修改更多资讯Item布局，增加资讯预览图片

--- andorid/SuE/app/src/main/java/org/hades/sue/adapter/MoreNewsAdapterOption.java
+++ andorid/SuE/app/src/main/java/org/hades/sue/adapter/MoreNewsAdapterOption.java
@@ -1,11 +1,13 @@
 package org.hades.sue.adapter;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.taro.headerrecycle.adapter.HeaderRecycleAdapter;
 import com.taro.headerrecycle.adapter.HeaderRecycleViewHolder;
 import com.taro.headerrecycle.adapter.SimpleRecycleAdapter;
@@ -35,15 +37,19 @@ public class MoreNewsAdapterOption extends SimpleRecycleAdapter.
         TextView date = holder.getView(R.id.news_date);
         //TODO:icon 保留
 
+        Glide.with(holder.getRootView())
+                .load(itemData.img)
+                .into(icon);
         title.setText(itemData.title);
         detail.setText(itemData.detail);
         String dateStr = itemData.date;
-        String year = dateStr.substring(0,3);
-        String month = dateStr.substring(4,5);
-        String day = dateStr.substring(6, 7);
+        String year = dateStr.substring(0,4);
+        String month = dateStr.substring(4,6);
+        String day = dateStr.substring(6, 8);
 
         String dateF = String.format(App.mContext.getString(R.string.news_date_f),
                 year, month, day);
+        Log.d("date", dateF);
         date.setText(dateF);
     }
 
--- andorid/SuE/app/src/main/java/org/hades/sue/bean/HeathNews.java
+++ andorid/SuE/app/src/main/java/org/hades/sue/bean/HeathNews.java
@@ -9,23 +9,27 @@ public class HeathNews implements Serializable{
     public String title;
     public String date;
     public String md5;
+    public String img;
 
-    public HeathNews(String link, String detail, String title, String date, String md5) {
+    public HeathNews(String link, String detail, String title, String date, String md5, String img) {
         this.link = link;
         this.detail = detail;
         this.title = title;
         this.date = date;
         this.md5 = md5;
+        this.img = img;
     }
 
     @Override
     public String toString() {
         return "HeathNews{" +
-                "link='" + link + '\'' +
+                "id=" + id +
+                ", link='" + link + '\'' +
                 ", detail='" + detail + '\'' +
                 ", title='" + title + '\'' +
                 ", date='" + date + '\'' +
                 ", md5='" + md5 + '\'' +
+                ", img='" + img + '\'' +
                 '}';
     }
 }
